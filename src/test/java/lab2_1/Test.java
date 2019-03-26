6f66a1da17870a0a569663e1287467d55654a548
, <202249@10.0.75.1>
2019-03-26 17:35:23 +0100

    testy_v1 Utworzenie testu, sprawdzającego czy jest w sekwencji przy długości 1

---
+++ src/test/java/lab2_1/Test.java
@@ -0,0 +1,20 @@
+package lab2_1;
+
+import static edu.iis.mto.bsearch.BinarySearch.search;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import edu.iis.mto.bsearch.SearchResult;
+
+public class Test {
+
+    private int singleElement[] = new int[1];
+
+    @org.junit.Test
+    public void IsInSeq() {
+        SearchResult result = search(singleElement[0], singleElement);
+        assertThat(result.isFound(), is(true));
+        assertThat(result.getPosition(), is(1));
+    }
+
+}
